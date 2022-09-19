063681c7b43664110391924d18f2c1dea5c6f726
manemejeff <kmb14056@yandex.ru>
2022-09-19 21:10:52 +0400

    Webpack-dev-server. Переменные окружения (env)

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
+import { buildDevServer } from './buildDevServer';
 
 import { buildLoaders } from './buildLoaders';
 import { buildPlugins } from './buildPlugins';
@@ -9,7 +10,7 @@ import { BuildOptions } from './types/config';
 export function buildWebpackConfig(
   options: BuildOptions
 ): webpack.Configuration {
-  const { paths, mode } = options;
+  const { paths, mode, isDev } = options;
 
   return {
     mode: mode,
@@ -24,5 +25,7 @@ export function buildWebpackConfig(
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 import { someFn } from './test';
 someFn(123);
+
+document.body.innerHTML = `<div>Hello world</div>`;
