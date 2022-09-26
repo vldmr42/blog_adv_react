339f5585ea5b3d9e52832e1a4ca9177df6840509
manemejeff <kmb14056@yandex.ru>
2022-09-26 20:52:11 +0400

    6 Роутинг Code splitting Lazy Suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import Counter from './components/Counter';
+import { Suspense } from 'react';
+import { Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import './index.scss';
+import AboutPage from './pages/AboutPage/AboutPage';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import MainPage from './pages/MainPage/MainPage';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 const App = () => {
   return (
     <div className="app">
-      qwerty
-      <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
+import { BrowserRouter } from 'react-router-dom';
 import { render } from 'react-dom';
 
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
