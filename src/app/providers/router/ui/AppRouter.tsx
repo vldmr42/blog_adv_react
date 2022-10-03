d8214e1cfe8f4609750953d9c25e9fc8bfdf7c20
manemejeff <kmb14056@yandex.ru>
2022-10-03 22:15:00 +0400

    21 Несуществующие маршруты. Лоадер для загрузки страниц

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,12 +1,11 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/ui/PageLoader';
 
 const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<PageLoader />}>
         <Routes>
             {Object.values(routeConfig).map(({ element, path }) => (
                 <Route
---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,23 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import './Loader.scss';
+
+interface LoaderProps {
+    className?: string;
+}
+
+export const Loader = ({ className }: LoaderProps) => (
+    <div className={classNames('lds-spinner', {}, [className])}>
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+    </div>
+);
