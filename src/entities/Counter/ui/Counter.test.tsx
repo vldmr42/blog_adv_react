d70fccdc6f85b2226c666776a9ecfb31c98c9d5e
manemejeff <kmb14056@yandex.ru>
2022-10-15 23:43:21 +0400

    Redux-toolkit. Entity. Тесты на всех уровнях

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,27 @@
+import { screen } from '@testing-library/react';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
+import userEvent from '@testing-library/user-event';
+import { Counter } from './Counter';
+
+describe('test Counter', () => {
+    test('render test', () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        expect(screen.getByTestId('value-title')).toHaveTextContent('10');
+    });
+    test('decrement test', () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        userEvent.click(screen.getByTestId('decrement-btn'));
+        expect(screen.getByTestId('value-title')).toHaveTextContent('9');
+    });
+    test('increment test', () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        userEvent.click(screen.getByTestId('increment-btn'));
+        expect(screen.getByTestId('value-title')).toHaveTextContent('11');
+    });
+});
---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,26 @@
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'shared/ui/Button/Button';
+import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { counterActions } from '../model/slice/counterSlice';
+
+export const Counter = () => {
+    const dispatch = useDispatch();
+    const counterValue = useSelector(getCounterValue);
+    const increment = () => {
+        dispatch(counterActions.increment());
+    };
+    const decrement = () => {
+        dispatch(counterActions.decrement());
+    };
+    const { t } = useTranslation();
+    return (
+        <div>
+            <h1 data-testid="value-title">
+                {counterValue}
+            </h1>
+            <Button data-testid="increment-btn" onClick={increment}>{t('Increment')}</Button>
+            <Button data-testid="decrement-btn" onClick={decrement}>{t('Decrement')}</Button>
+        </div>
+    );
+};
