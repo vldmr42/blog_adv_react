747f5e24a80adae31c3725f44149259f673dd6ac
manemejeff <kmb14056@yandex.ru>
2022-12-11 12:53:25 +0400

    43 Переменная __PROJECT__. Тесты на модуль профиля p2

---
+++ src/entities/Profile/model/slice/profileSlice.test.ts
@@ -0,0 +1,56 @@
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+import { updateProfileData } from '../services/updateProfileData/updateProfileData';
+import { ProfileSchema, ValidateProfileError } from '../types/profile';
+import { profileActions, profileReducer } from './profileSlice';
+
+const profileData = {
+    username: 'admin',
+    age: 22,
+    country: Country.Armenia,
+    lastname: 'last',
+    first: 'first',
+    city: 'Rome',
+    currency: Currency.EUR,
+};
+
+describe('profileSlice.test', () => {
+    test('test set readonly', () => {
+        const state: DeepPartial<ProfileSchema> = { readonly: false };
+        expect(profileReducer(
+            state as ProfileSchema,
+            profileActions.setReadonly(true),
+        )).toEqual({ readonly: true });
+    });
+
+    test('test cancel edit', () => {
+        const state: DeepPartial<ProfileSchema> = { data: profileData, form: { username: '' } };
+        expect(profileReducer(
+            state as ProfileSchema,
+            profileActions.cancelEdit(),
+        )).toEqual({
+            readonly: true, validateErrors: undefined, data: profileData, form: profileData,
+        });
+    });
+
+    test('test update profile', () => {
+        const state: DeepPartial<ProfileSchema> = { form: { username: 'test', first: 'test' } };
+        expect(profileReducer(
+            state as ProfileSchema,
+            profileActions.updateProfile(profileData),
+        )).toEqual({
+            form: profileData,
+        });
+    });
+
+    test('test update profile service pending', () => {
+        const state: DeepPartial<ProfileSchema> = { isLoading: false, validateErrors: [ValidateProfileError.SERVER_ERROR] };
+        expect(profileReducer(
+            state as ProfileSchema,
+            updateProfileData.pending,
+        )).toEqual({
+            isLoading: true,
+            validateErrors: undefined,
+        });
+    });
+});
