3c63b2eb32e089181ae5a130d93437782df356c3
manemejeff <kmb14056@yandex.ru>
2023-02-02 00:06:51 +0400

    62 CopyPlugin. Подготовка к продакшну. Публикуем на Netlify p3 fix api

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { config } from 'process';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 export const $api = axios.create({
@@ -6,4 +7,13 @@ export const $api = axios.create({
     headers: {
         authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
     },
+});
+
+$api.interceptors.request.use(() => {
+    // @ts-ignore
+    if (config.headers) {
+        // @ts-ignore
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+    return config;
 });
