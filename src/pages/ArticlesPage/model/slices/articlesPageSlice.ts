dbebfa1bcf9412917c0ab17f192082ed5003bf2a
manemejeff <kmb14056@yandex.ru>
2023-05-24 08:33:05 +0400

    fix SortOrder import

--- src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
+++ src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
@@ -12,7 +12,7 @@ import {
     ArticleType,
 } from '@/entities/Article';
 import { ARTICLE_VIEW_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
-import { SortOrder } from '@/shared/types';
+import { SortOrder } from '@/shared/types/sort';
 
 import { fetchArticlesList } from '../services/fetchArticlesList/fetchArticlesList';
 import { ArticlesPageSchema } from '../types/articlesPageSchema';
