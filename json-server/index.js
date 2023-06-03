e103f54f520b2549ff5c9fe65d54f3e2e6f3a2c0
manemejeff <kmb14056@yandex.ru>
2023-06-03 12:45:18 +0400

    121 SSL. Сертификат. Настраиваем https

--- json-server/index.js
+++ json-server/index.js
@@ -1,6 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
-const jsonServer = require('json-server');
+const http = require('http');
+const https = require('https');
 const path = require('path');
+
+const jsonServer = require('json-server');
+
+const options = {
+    key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+    cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+};
 
 const server = jsonServer.create();
 
@@ -21,7 +30,9 @@ server.use(async (req, res, next) => {
 server.post('/login', (req, res) => {
     try {
         const { username, password } = req.body;
-        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+        const db = JSON.parse(
+            fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'),
+        );
         const { users = [] } = db;
 
         const userFromBd = users.find(
@@ -52,6 +63,16 @@ server.use((req, res, next) => {
 server.use(router);
 
 // запуск сервера
-server.listen(8000, () => {
-    console.log('server is running on 8000 port');
+const PORT = 8443;
+const HTTP_PORT = 8000;
+
+const httpsServer = https.createServer(options, server);
+const httpServer = http.createServer(server);
+
+httpsServer.listen(PORT, () => {
+    console.log(`server is running on ${PORT} port`);
+});
+
+httpServer.listen(HTTP_PORT, () => {
+    console.log(`server is running on ${HTTP_PORT} port`);
 });
