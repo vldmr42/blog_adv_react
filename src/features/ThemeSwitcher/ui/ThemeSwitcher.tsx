0325d19dbfa19aa7904ed9161467a2f90d9d0be1
manemejeff <kmb14056@yandex.ru>
2023-06-21 22:17:43 +0400

    132 SVGR. Обработка размеров и цветов иконки на этапе сборки

--- src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,13 +1,12 @@
 import { useCallback } from 'react';
 
 import { saveJsonSettings } from '@/entities/User';
-import DarkIcon from '@/shared/assets/icons/theme-dark.svg';
-import LightIcon from '@/shared/assets/icons/theme-light.svg';
-import { Themes } from '@/shared/const/theme';
+import ThemeIcon from '@/shared/assets/icons/theme-light.svg';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 import { Button, ButtonTheme } from '@/shared/ui/Button';
+import { Icon } from '@/shared/ui/Icon';
 
 import cls from './ThemeSwitcher.module.scss';
 
@@ -30,7 +29,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
             onClick={onToggleHandler}
             className={classNames(cls.ThemeSwitcher, {}, [className])}
         >
-            {theme === Themes.DARK ? <DarkIcon /> : <LightIcon />}
+            <Icon Svg={ThemeIcon} width={40} height={40} inverted />
         </Button>
     );
 };
